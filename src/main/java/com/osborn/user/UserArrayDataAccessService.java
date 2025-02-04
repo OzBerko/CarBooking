7859342fc8f38c22ce6d23c15e21f44d1eedb56e
OsB <berko.akwasi@gmail.com>
2025-02-04 00:00:51 -0500

    Added usage of javafaker - java faker is used to provide user.csv file

--- src/main/java/com/osborn/user/UserArrayDataAccessService.java
+++ src/main/java/com/osborn/user/UserArrayDataAccessService.java
@@ -1,26 +1,59 @@
 package com.osborn.user;
 
+import com.github.javafaker.Faker;
+
 import java.io.*;
 import java.util.*;
 
 public class UserArrayDataAccessService implements UserDao {
-//    private final static File file = new File("src/main/resources/users.csv");
-    private final static File file = new File(Objects.requireNonNull(UserArrayDataAccessService.class.getClassLoader()
-                                                                                                     .getResource(
-                                                                                                             "users.csv"))
-                                                     .getPath());
+    private final static String filePath = "target/runtime-files/users.csv";
+    private final static File file = new File(filePath);
+    //    private final static File file1 = new File(filePath);
+//    private final static File file = new File(Objects.requireNonNull(UserArrayDataAccessService.class.getClassLoader()
+//                                                                                                     .getResource(
+//                                                                                                             "users.csv"))
+//                                                     .getPath());
 
     private final static Map<Integer, User> users = new HashMap<>();
 
     private static Integer usersIndex = 0;
 
     static {
+        generateUsersCsv();
         loadUsersFromFile();
+        printFile();
+    }
+
+    private static void printFile() {
+        System.out.println("\nUSER FILE CHECK:");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    private static void generateUsersCsv() {
+        Faker faker = new Faker();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            for (int i = 0; i < 10; i++) {
+                writer.write(UUID.randomUUID() + ", "
+                             + faker.name()
+                                    .firstName() + ", "
+                             + faker.name()
+                                    .lastName() + "\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 
     private static void loadUsersFromFile() {
         try (Scanner scanner = new Scanner(file)) {
-            if(!scanner.hasNextLine()){
+            if (!scanner.hasNextLine()) {
                 System.out.println("Empty users file: No valid users found in the file.");
             }
             while (scanner.hasNextLine()) {
